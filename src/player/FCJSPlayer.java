99c47bf322e4ccc50505270124466d9961b243f1
Vaz <fccinelli@wpi.edu>
2016-01-22 20:15:21 -0500

    Implemented all the logic for input reading, though it's not yet tested. Wrote a function to apply a move to our board to keep track of the board state.

--- src/player/FCJSPlayer.java
+++ src/player/FCJSPlayer.java
@@ -1,3 +1,7 @@
+/** @author Forrest Cinelli
+ *  @author Jingwei Shen
+ * */
+
 package player;
 
 import java.io.BufferedReader;
@@ -7,20 +11,45 @@ import java.util.Arrays;
 import java.util.List;
 
 /* Questions for ta:
- * why does running referee cause an error?
- * how can our player know how much time it has to make a move? Similarly, how can our player know waht the size of the board is?
  * can we modify board.java?
  * is there a size limit on the .zip file we submit? is there a limit on how much RAM our program uses?
  * */
 
-public class FCJSPlayer {
+public class FCJSPlayer 
+{
 	public String playerName = "FCJS";
 	public int playerNumber;
+	public int opponentNumber;
 	public Board board; //current state of board
+	BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+	boolean first_move=false;
+	int timeLimit; //in seconds
+	List<String> currentBestMove = Arrays.asList("0 1".split(" ")); //move formatting: first number is column to move in. (the first column is numbered 0, and the last is width - 1). Second number is move type: 0 means pop out and 1 means drop in. 
 	
 	public FCJSPlayer()
 	{
 		
+	}
+	
+	//given a move, apply it to our board. Should happen after opponent declares move but before we begin searching the game tree and again after a move is returned.
+	public void applyMove(List<String> move, Board b, int player)
+	{
+		if (Integer.parseInt(move.get(1)) == 0) //if move is a drop in
+		{
+			board.dropADiscFromTop(Integer.parseInt(move.get(0)), opponentNumber);
+		}
+		else //move is a pop out
+		{
+			board.removeADiscFromBottom(Integer.parseInt(move.get(0)));
+		}
+	}
+	
+	/* should be ready to return a move at any moment.*/
+	//this needs to be implemented differently; to allow for time limiting, and also to not waste time updating our own board before the move is sent out.
+	public String getNextMove()
+	{
+		applyMove(currentBestMove, board, playerNumber);
+		return String.join(" ", currentBestMove);
 	}
 	
 	/* Does alpha beta pruning */
@@ -46,40 +75,57 @@ public class FCJSPlayer {
 		return 0;
 	}
 	
-	BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
-	boolean first_move=false;
-	
+	//this whole thing reads the input and updates info in our player appropriately. 
 	public void processInput() throws IOException
 	{	
 	
     	String s=input.readLine();	
 		List<String> ls=Arrays.asList(s.split(" "));
-		if(ls.size()==2){
-			System.out.println(ls.get(0)+" "+ls.get(1));
+		if(ls.size()==2)
+		{
+			applyMove(ls, board, opponentNumber);
 		}
-		else if(ls.size()==1){
-			System.out.println("game over!!!");
+		else if(ls.size()==1)
+		{
+			System.out.println("ggwp");
 			System.exit(0);
 		}
-		else if(ls.size()==5){          //ls contains game info
+		else if(ls.size()==5)
+		{          //ls contains game info
 			board = new Board(Integer.parseInt(ls.get(0)), //height 
 							  Integer.parseInt(ls.get(1)), //width
 							  Integer.parseInt(ls.get(2))  //N
 							  );
-			System.out.println("0 1");  //first move
+			if (Integer.parseInt(ls.get(3)) == playerNumber) //whose move is it first
+			{
+				first_move = true;
+			}
+			else
+			{
+				first_move = false;
+			}
+			timeLimit = Integer.parseInt(ls.get(4));
+			
+			//right now this is not correct. We need to return before time is up. 
+			System.out.println(this.getNextMove());  //first move
 		}
-		else if(ls.size()==4){		//player1: aa player2: bb
-			if (ls.get(1).equals(playerName))
+		else if(ls.size()==4)
+		{		//player1: aa player2: bb
+			if ((ls.get(1)).equals(playerName))
 			{
 				playerNumber = 1;
+				opponentNumber = 2;
 			}
 			else 
 			{
 				playerNumber = 2;
+				opponentNumber = 1;
 			}
 		}
 		else
+		{
 			System.out.println("not an input I understand");
+		}
 	}
 	
 	public static void main(String[] args) throws IOException 
